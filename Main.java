573b27f36f4f93ff0c5d74d912d690626362cf7c
Ilya Antonov <da0201@mail.ru>
2025-07-06 11:40:34 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите цену товара (в руб.):");
+        int x = scanner.nextInt();
+
+        System.out.print("Введите вес товара (в кг.):");
+        int y = scanner.nextInt();
+
+        calculateCustoms(x, y);
+
+    }
+    public static void calculateCustoms(int x, int y){
+        double result = x / 100;
+        int result2 = y * 100;
+        double c = result + result2;
+        System.out.println("Размер пошлины (в руб.) составит:" + c);
+
+
+
+
+
+    }
+}
